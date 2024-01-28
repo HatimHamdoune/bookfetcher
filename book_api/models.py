1601f5e2ee7dd7b6812d944d9f6e6e195f012933
crowbar <hatimhamdoune@outlook.com>
2024-01-28 22:39:06 +0100

    Define api path route

--- book_api/models.py
+++ book_api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Modal):
-    title = models.Charfield(max_length=100)
+class Book(models.Model):
+    title = models.CharField(max_length=100)
     number_of_pages = models.IntegerField()
     publish_date = models.DateField()
     quantity = models.IntegerField
